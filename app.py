0b1fa6ff2cb45db69daf6a942c0c8ee822db11c8
Vincent Kriek <vincent@kriek.org>
2022-03-17 21:48:21 +0100

    Add label overlaid over image

--- app.py
+++ app.py
@@ -1,29 +1,42 @@
+import os
+
 from images import RawFolder
 
 from kivy.app import App
 from kivy.core.window import Window
 from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.uix.floatlayout import FloatLayout
 
-class Photo(Image):
+class Photo(FloatLayout):
     def __init__(self, **kwargs):
         super(Photo, self).__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._keyboard_closed, self)
         self._keyboard.bind(on_key_down=self._on_keyboard_down)
         self._files = RawFolder()
-        self.source = self._files.get_next_image()
+        self.label = Label(pos_hint={'center_x': 0.1, 'center_y': 0.9})
+        self.image = Image()
+        self.add_widget(self.image)
+        self.add_widget(self.label)
+
+        self._update_image(self._files.get_next_image())
 
     def _keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_keyboard_down)
         self._keyboard = None
+
+    def _update_image(self, path):
+        self.image.source = path
+        self.label.text = os.path.basename(path)
 
     def _on_keyboard_down(self, keyboard, keycode, text, modifiers):
         print(keycode)
         if keycode[1] == "q":
             App.get_running_app().stop()
         if keycode[1] == "right":
-            self.source = self._files.get_next_image()
+            self._update_image(self._files.get_next_image())
         if keycode[1] == "left":
-            self.source = self._files.get_prev_image()
+            self._update_image(self._files.get_prev_image())
 
         return True
 
